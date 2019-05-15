75b49498e8d53318122804a1d2c50d83a4950110
sarahkittyy <chrisohlin1@gmail.com>
2019-05-14 22:05:48 -0400

    replaced push() with just function()

--- include/Scripts/Script.hpp
+++ include/Scripts/Script.hpp
@@ -52,77 +52,23 @@ public:
 	}
 
 	/**
-	 * @brief Push data into a lua global table. 
+	 * @brief Push a function to the lua state.
+	 * This is used for setters and getters, such as set/getPos(),
+	 * so that the lua state doesn't have to be modified on every lua script run. 
 	 * 
-	 * @tparam T Some uniform value type that all properties of the return value return.
-	 * 
-	 * @param table The table to put the property into.
-	 * @param name The name of the property.
-	 * @param returns The list of data properties
-	 * 
-	 * @remarks table being "_G" will make `name()` global.
-	 * 
-	 * @remarks For example, if:
-	 * table = table1
-	 * name = data,
-	 * returns = unordered_map is {x, 1},
-	 * then it will be accessible in lua as table1.data().x == 1 
-	 */
-	template <typename T>
-	void push(std::string table, std::string name, std::unordered_map<std::string, T> returns)
-	{
-		//Create the table and set the property.
-		sol::table t = getCreateTable(table);
-		//Create the return value with the values in `returns`
-		sol::table ret = mState.create_table();
-		for (auto& [key, value] : returns)
-		{
-			//Push the value into the table.
-			ret[key] = value;
-		}
-		//Set the function `name()` to return table `ret`
-		t.set_function(name, [ret]() -> decltype(ret) { return ret; });
-	}
-
-	/**
-	 * @brief Push a value in the given table, under the given property name, with the given value.
-	 * 
-	 * @tparam T The value to return from the property getter.
-	 * 
-	 * @param table The table to push `name` into.
-	 * @param name The name of the property.
-	 * @param returns The value that name will equal.
-	 * 
-	 * @remarks table being "_G" will make `name` global.
-	 * 
-	 * @see push() (the other overload).
-	 */
-	template <typename T>
-	void push(std::string table, std::string name, const T& returns)
-	{
-		//Create the table & set the property.
-		sol::table t  = getCreateTable(table);
-		t[name]		  = [returns]() -> T { return returns; };
-		mState[table] = t;
-	}
-
-	/**
-	 * @brief Append a function to the script.
-	 * 
-	 * @tparam Fn The lambda function type to append (usually deduced)
-	 * 
-	 * @param table The table to append the function to ("_G" for global.)
-	 * @param name The name of the function. 
-	 * @param fn The function itself.
+	 * @tparam Fn Lambda expression.
+	 * @param table The name of the table to push the function into
+	 * @param name The name of the function
+	 * @param value The function itself
 	 */
 	template <typename Fn>
-	void function(std::string table, std::string name, const Fn& fn)
+	void function(std::string table, std::string name, const Fn& value)
 	{
-		//Create the table.
+		//Get the table
 		sol::table t = getCreateTable(table);
-		//Set the function
-		t.set_function(name, fn);
-		//Set the table.
+		//Set the table function
+		t.set_function(name, value);
+		//Push the table to the state.
 		mState[table] = t;
 	}
 
